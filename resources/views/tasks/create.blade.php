d0cc07d4067016bf69cd932cb89707bc177e2cec
EC2 Default User <ec2-user@ip-172-31-73-137.ec2.internal>
2021-06-22 08:52:23 +0000

    tasklist design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -3,17 +3,31 @@
 @section('content')
 
     <h1>タスク新規作成ページ</h1>
+    
+    <div class="row">
+        <div class="col-xs-12 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
 
-    {!! Form::model($task, ['route' => 'tasks.store']) !!}
+        {!! Form::model($task, ['route' => 'tasks.store']) !!}
+    
+        <div class="form-group">
 
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
+            {!! Form::label('status', 'ステータス:') !!}
+            {!! Form::text('status', null, ['class' => 'form-control']) !!}
+        
+        </div>
 
+
+        <div class="form-group">
+        
         {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
+        {!! Form::text('content', null, ['class' => 'form-control']) !!}
+        </div>
 
-        {!! Form::submit('登録') !!}
+            {!! Form::submit('登録', ['class' => 'btn btn-primary']) !!}
 
-    {!! Form::close() !!}
+        {!! Form::close() !!}
+
+        </div>
+    </div>
 
 @endsection
